4302cd49cbde8dcb3fe2631225163872904cba8e
Cobbie Quintos <cobbiequintos@gmail.com>
2020-04-14 19:41:47 +0800

    about content, except last

--- pages/index.vue
+++ pages/index.vue
@@ -13,10 +13,22 @@
   <div class="about-me">
     <h1 class="about-header">What I like to do</h1>
     <div class="about-grid">
-      <div class="about-grid-item item-1">Item 1</div>
-      <div class="about-grid-item item-2">Item 2</div>
-      <div class="about-grid-item item-3">Item 3</div>
-      <div class="about-grid-item item-4">Item 4</div>
+      <div class="about-grid-item item-1">
+        <img class="about-item-img" src="~/assets/idea.svg" alt="">
+        <h1 class="about-item-header"><span id="code">Code</span> Learn & build things</h1>
+        <p class="about-item-p">I developed a passion for coding ever since I entered college. Dealing mostly with front-end development, my main experience lies with JavaScript, VueJS, and Node, but I also consider myself to be a pretty okay Python programmer.</p>
+      </div>
+      <div class="about-grid-item item-2"><img class="about-item-img" src="~/assets/magichat.svg" alt="">
+        <h1 class="about-item-header"><span id="code">Games</span> Adventures</h1>
+        <p class="about-item-p">One of the things I'm most thankful for is that I was lucky enough to grow up in a time where gaming (and technology in general) really started to take off. Some of my favorite titles/franchises are Overwatch, Apex Legends, Horizon Zero Dawn, Call of Duty, and Pokemon.</p></div>
+      <div class="about-grid-item item-3"><img class="about-item-img" src="~/assets/handshake.svg" alt="">
+        <h1 class="about-item-header"><span id="code">Networking</span> Meeting people</h1>
+        <p class="about-item-p">I'm an introvert. But I've had the privilege of being able to go on two life-changing experiences abroad
+          during my stay in college. Among the many things I learned in these adventures, here's one of the most valuable lessons: there 
+          are so many things you can only learn by meeting and talking to other people, especially from different backgrounds and places.</p></div>
+      <div class="about-grid-item item-4"><img class="about-item-img" src="~/assets/idea.svg" alt="">
+        <h1 class="about-item-header"><span id="code">Code</span> Build things</h1>
+        <p class="about-item-p"></p></div>
     </div>
   </div></div>
 </template>
@@ -107,7 +119,12 @@ export default {
 }
 
 .about-grid-item{
-
+  display: grid;
+  grid-template-columns: 20% 5% 75%;
+  grid-template-rows: 20% 80%;
+  grid-template-areas: 
+  "image . header " 
+  ". . body ";
 }
 
 .item-1{
@@ -127,5 +144,27 @@ export default {
 }
 .item-4{
   /* grid-area: fourth-item */
+}
+
+.about-grid-item>h1{
+  font-weight: normal;
+}
+.about-item-img{
+  grid-area: image;
+  justify-self: right;
+  align-self: right;
+}
+.about-item-p{
+  grid-area: body;
+  text-align: left;
+  padding-top: 10px;
+  line-height: 22px;
+}
+.about-item-header{
+  grid-area: header;
+  text-align: left;
+}
+#code{
+  text-decoration: line-through
 }
 </style>
